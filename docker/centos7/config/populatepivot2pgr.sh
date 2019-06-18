3ed2ef7c29b1cf08b1589a61138d4e2f5c616322
azarzelli <amaury.zarzelli@ign.fr>
2019-06-18 13:52:34 +0200

    Meilleurs argument r2gg

--- docker/centos7/config/populatepivot2pgr.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-R2GG_ARG=$1
-
-r2gg:populate_pivot ${R2GG_ARG}
-r2gg:pivot2pgrouting ${R2GG_ARG}
---
+++ docker/centos7/config/r2gg_pipeline.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+r2gg:populate_pivot $R2GG_ARG
+
+if [ $GENERATION_TYPE = "osrm" ]; then
+  r2gg:pivot2osrm $R2GG_ARG
+elif [ $GENERATION_TYPE = "pgr" ]; then
+  r2gg:pivot2osrm $R2GG_ARG
+fi
