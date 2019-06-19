f4bb19dc947a7d81ef0e57cf7d55f0a7a05dd7c1
Loic Grondin <loic.grondin@ign.fr>
2019-06-19 14:44:51 +0200

    correction du r2gg_pipeline

--- docker/centos7/config/r2gg_pipeline.sh
+++ docker/centos7/config/r2gg_pipeline.sh
@@ -5,5 +5,5 @@ r2gg:populate_pivot $R2GG_ARG
 if [ $GENERATION_TYPE = "osrm" ]; then
   r2gg:pivot2osrm $R2GG_ARG
 elif [ $GENERATION_TYPE = "pgr" ]; then
-  r2gg:pivot2osrm $R2GG_ARG
+  r2gg:pivot2pgrouting $R2GG_ARG
 fi
