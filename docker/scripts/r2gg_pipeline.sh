726b85d8808946cfe1093f036794a09328660c92
Loic <loic.grondin@ign.fr>
2021-09-07 15:29:08 +0200

    fix : idée sur les noms de variables pour le bash

--- docker/scripts/r2gg_pipeline.sh
+++ docker/scripts/r2gg_pipeline.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env sh
 
-# TODO : On analyse certaines VARENV 
-## S'il n'y a rien, on fait une génération à partir de données OSM sur la Corse
 
-## S'il y a un nom de donnée OSM, on fait cette génération
+# ## S'il n'y a rien, on fait une génération à partir de données OSM sur la Corse en utilisant un fichier de configuration déjà présent dans l'image docker
+# if [ $R2GG_GENERATION_CONF = "" ]; then
+  
+# ## S'il y a une configuration, alors on donne cette configuration à r2gg qui se chargera de l'analyser et d'agir en conséquence 
+# else
 
-## S'il y a un fichier de configuration, on lance avec ce fichier
+# fi
 
-
-# On lance la génération 
+# # On lance la génération 
 r2gg:populate_pivot $R2GG_ARG
 
 if [ $GENERATION_TYPE = "osrm" ]; then
