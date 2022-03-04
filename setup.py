2d223ee0bc16b8bd35b1223c8df0502a8b39033d
amaury.zarzelli <amaury.zarzelli@ign.fr>
2022-03-04 15:30:19 +0100

    update versions + changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
     name = 'r2gg',
-    version = '1.1.1-DEVELOP',
+    version = '1.1.2',
     entry_points = {
             'console_scripts': [
                 'r2gg-populate_pivot=r2gg:populate_pivot',
