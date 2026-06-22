cb8a524708ee8109543d0ac3279a923a8f005656
azarz <amaury.zarzelli@gmail.com>
2026-06-22 16:27:17 +0200

    ajustements

--- README.md
+++ README.md
@@ -1,12 +1,38 @@
 # Route Graph Generator
 
-## Présentation 
+## Présentation
 
-Route Graph Generator (r2gg) est un script Python qui permet la génération de graphes pour des moteurs de calcul d'itinéraire. Il a été développé pour générer les données directement utilisable par [Road2](https://github.com/IGNF/road2). 
+Route Graph Generator (r2gg) est un script Python qui permet la génération de graphes pour des moteurs de calcul d'itinéraire. Il a été développé pour générer les données directement utilisable par [Road2](https://github.com/IGNF/road2).
 
-Actuellement, il y a trois formats de sortie : OSRM, pgRouting et Valhalla. 
+Actuellement, il y a trois formats de sortie : OSRM, pgRouting et Valhalla.
 
 La conversion se fait via les fonctions de la bibliothèque r2gg développée dans ce but. Une documentation plus détaillée de r2gg est consultable [ici](https://ignf.github.io/route-graph-generator/).
+
+## Génerer des données en local
+
+Un docker compose est mis à disposition pour générer des données Valhalla en local.
+
+```
+UID=$(id -u) GID=$(id -g) docker compose up --build
+```
+
+Attendre l'import de la donnée, puis
+
+```
+docker compose exec r2gg sh -c "r2gg-sql2pivot docker/config/bdtopo2valhalla_local.json && r2gg-pivot2osm docker/config/bdtopo2valhalla_local.json && r2gg-osm2valhalla docker/config/bdtopo2valhalla_local.json && r2gg-road2config docker/config/bdtopo2valhalla_local.json"
+```
+
+Les données se trouveront dans le dossier ./data/generation
+
+Pour faire tourner un service test valhalla sur les données ainsi générées, utiliser les commandes suivante:
+```
+docker run -dit valhalla -p 8002:8002 -v ${PWD}/data/generation:/custom_files -e use_tiles_ignore_pbf=True -e force_rebuild=False -e build_admins=False ghcr.io/valhalla/valhalla-scripted:latest
+```
+
+Puis checker la création d'itinéraire via le lien suivant
+```
+http://localhost:8002/route?json={"costing":"pedestrian","costing_options":{"pedestrian":{"use_ferry":0,"use_living_streets":0.5,"use_tracks":0,"private_access_penalty":450,"destination_only_penalty":300,"elevator_penalty":120,"service_penalty":15,"service_factor":1,"shortest":false,"type":"Foot","use_hills":0.5,"walking_speed":5.1,"walkway_factor":1,"sidewalk_factor":1,"alley_factor":2,"driveway_factor":5,"step_penalty":0,"max_hiking_difficulty":1,"use_lit":0,"transit_start_end_max_distance":2145,"transit_transfer_max_distance":800}},"exclude_polygons":[],"locations":[{"lon":2.320041,"lat":48.8588897,"type":"break"},{"lon":2.3380277,"lat":48.8611473,"type":"break"}],"units":"kilometers","alternates":2,"id":"valhalla_directions","language":"fr-FR","directions_options":{"format":"osrm"},"shape_format":"geojson","format":"osrm"}
+```
 
 ## Prérequis
 
@@ -44,9 +70,9 @@ pip3 install --user -e .
 
 Pour pouvoir lancer les scripts de génération, il faut définir une configuration (au format JSON) par ressource à générer. Ce fichier de configuration fait références à d'autres fichiers de configuration : pour la gestion des logs, la gestion des connexions aux bases de données, et pour le calcul des coûts.
 Des exemples de tous ces fichiers sont présents dans le dépôt dans le dossier `io`.
-La documentation de ces fichiers de configuration est consultable [ici](https://github.com/IGNF/route-graph-generator/tree/master/io). 
+La documentation de ces fichiers de configuration est consultable [ici](https://github.com/IGNF/route-graph-generator/tree/master/io).
 
-Un exemple de ces fichiers est disponible dans la partie [docker](https://github.com/IGNF/route-graph-generator/tree/master/docker/config). 
+Un exemple de ces fichiers est disponible dans la partie [docker](https://github.com/IGNF/route-graph-generator/tree/master/docker/config).
 
 ### Exécution
 
@@ -94,6 +120,6 @@ Elle est indiquée dans le `__about__.py`.
 
 Route-graph-generator est diffusé sous la licence GPL v3.
 
-## Participer aux développements 
+## Participer aux développements
 
-Les participations à ce projet sont encouragées (votre notre [charte](./CODE_OF_CONDUCT.md) à ce sujet). Nous avons mis en place un [guide](./CONTRIBUTING.md) des contributions pour vous accompagner dans cette démarche. 
+Les participations à ce projet sont encouragées (votre notre [charte](./CODE_OF_CONDUCT.md) à ce sujet). Nous avons mis en place un [guide](./CONTRIBUTING.md) des contributions pour vous accompagner dans cette démarche.
---
+++ sql/bdtopo_v3.3_local.sql
@@ -0,0 +1,407 @@
+-- ##########################################################
+-- IMPORT DE LA TABLE DES TRONCON DE ROUTE DEPUIS BDTOPO V3.3
+-- ##########################################################
+
+CREATE SCHEMA IF NOT EXISTS {output_schema};
+
+-- ####################################
+-- DEFINITION DU GRAPHE : NOEUDS / ARCS
+-- ####################################
+-- répétition du IF EXISTS pour ouvrir à l'éventualité de la mise à jour
+DROP TABLE IF EXISTS {output_schema}.nodes CASCADE ;
+CREATE TABLE IF NOT EXISTS {output_schema}.nodes (
+  id bigserial primary key,
+  lon double precision,
+  lat double precision,
+  geom geometry(Point,4326)
+);
+
+-- répétition du IF EXISTS pour ouvrir à l'éventualité de la mise à jour
+DROP SEQUENCE IF EXISTS {output_schema}.edges_id_seq;
+CREATE SEQUENCE IF NOT EXISTS {output_schema}.edges_id_seq;
+DROP TABLE IF EXISTS {output_schema}.edges;
+CREATE TABLE IF NOT EXISTS {output_schema}.edges (
+  id bigserial primary key,
+  source_id bigserial,
+  target_id bigserial,
+  x1 double precision,
+  y1 double precision,
+  x2 double precision,
+  y2 double precision,
+  length_m double precision,
+  direction integer,
+  geom geometry(Linestring,4326),
+  -- Attributs spécifiques à la bd uni
+  vitesse_moyenne_vl integer NOT NULL DEFAULT 0,
+  nature text,
+  cleabs text,
+  importance integer,
+  way_names text,
+  nom_1_gauche text,
+  nom_1_droite text,
+  cpx_numero text,
+  cpx_toponyme_route_nommee text,
+  sens_de_circulation text,
+  position_par_rapport_au_sol integer,
+  acces_vehicule_leger text,
+  largeur_de_chaussee double precision,
+  nombre_de_voies text,
+  insee_commune_gauche text,
+  insee_commune_droite text,
+  itineraire_vert boolean,
+  reserve_aux_bus text,
+  urbain boolean NOT NULL DEFAULT false,
+  acces_pieton text,
+  nature_de_la_restriction text,
+  restriction_de_hauteur double precision,
+  restriction_de_poids_total double precision,
+  restriction_de_poids_par_essieu double precision,
+  restriction_de_largeur double precision,
+  restriction_de_longueur double precision,
+  matieres_dangereuses_interdites boolean,
+  cpx_gestionnaire text,
+  cpx_numero_route_europeenne text,
+  cpx_classement_administratif text,
+  transport_exceptionnel boolean,
+  vla_par_defaut integer,
+  cout_penalites numeric,
+  vehicule_leger_interdit boolean,
+  cout_vehicule_prioritaire numeric
+);
+
+
+CREATE INDEX IF NOT EXISTS nodes_geom_gist ON {output_schema}.nodes USING GIST (geom);
+CREATE INDEX IF NOT EXISTS nodes_lon_lat_idx ON {output_schema}.nodes(lon,lat);
+CREATE INDEX IF NOT EXISTS edges_geom_gist ON {output_schema}.edges USING GIST (geom);
+
+-- ####################################
+-- UTILITAIRES DE REMPLISSAGE DU GRAPHE
+-- ####################################
+
+-- Récupération d'un identifiant de sommet en fonction d'un point (égalité stricte)
+CREATE OR REPLACE FUNCTION {output_schema}.nodes_id( _geom geometry ) RETURNS bigint AS $$
+  SELECT id FROM {output_schema}.nodes WHERE lon = ST_X(_geom) AND lat = ST_Y(_geom);
+$$ LANGUAGE SQL ;
+
+-- Renvoie les points intermédiaires d'une linestring en format json
+CREATE OR REPLACE FUNCTION {output_schema}.inter_nodes(geom geometry(LineString, 4326)) RETURNS json[] AS $$
+  SELECT COALESCE(array_agg(row_to_json(subq)), '{{}}') FROM (
+    SELECT
+      ST_X((dp).geom) AS lon,
+      ST_Y((dp).geom) AS lat
+    FROM (
+      SELECT st_numpoints(geom) AS nump, ST_DumpPoints(geom) AS dp
+    ) AS foo
+    WHERE (dp).path[1] <@ int4range(2, nump)
+  ) subq ;
+$$ LANGUAGE SQL;
+
+-- populate.sql
+-- On doit copier au préalable troncon_de_route pour figer les tronçons car elle est mise à jour en continue
+
+CREATE TEMP TABLE IF NOT EXISTS bduni_non_com_tmp AS
+SELECT * FROM {output_schema}.non_communication;
+
+CREATE TEMP TABLE troncon_de_route_local AS
+SELECT
+  *
+FROM {output_schema}.troncon_de_route;
+
+
+-- ############################
+-- REMPLISSAGE DE BDUNI_TRONCON
+-- ############################
+-- Ajout des tronçons de routes (jointure avec routes numérotées et nommées)
+CREATE TEMP TABLE IF NOT EXISTS bduni_troncon AS
+  SELECT * FROM (
+    SELECT
+      -- GCVS (système d'historique)
+    t.cleabs as cleabs,
+    -- t.gcms_detruit AS detruit,
+    -- t.gcms_territoire as territoire,
+
+    -- BD TOPO
+    t.etat_de_l_objet as etat,
+    t.nature as nature,
+    NULLIF(t.importance,'')::int as importance,
+    t.sens_de_circulation as sens_de_circulation,
+    (CASE
+    WHEN t.vitesse_moyenne_vl=1 THEN 0
+    WHEN t.vitesse_moyenne_vl IS NULL  THEN 0
+    ELSE t.vitesse_moyenne_vl::integer
+    END) as vitesse_moyenne_vl,
+
+    -- Pour l'attribut name
+    t.nom_collaboratif_gauche as nom_1_gauche,
+    t.nom_collaboratif_droite as nom_1_droite,
+    t.cpx_numero as cpx_numero,
+    t.cpx_toponyme_route_nommee as cpx_toponyme,
+
+    (CASE
+    WHEN t.position_par_rapport_au_sol='Gué ou radier' THEN 0
+    ELSE t.position_par_rapport_au_sol::integer
+    END) as position_par_rapport_au_sol,
+    t.acces_vehicule_leger as acces_vehicule_leger,
+
+    t.largeur_de_chaussee as largeur_de_chaussee,
+
+    -- Champs demandés par la DP
+    t.itineraire_vert as itineraire_vert,
+    t.nombre_de_voies as nombre_de_voies,
+    t.insee_commune_gauche as insee_commune_gauche,
+    t.insee_commune_droite as insee_commune_droite,
+    t.reserve_aux_bus as reserve_aux_bus,
+    (CASE
+    WHEN t.urbain IS NULL THEN false
+    ELSE t.urbain
+    END) as urbain,
+    t.acces_pieton as acces_pieton,
+    t.nature_de_la_restriction as nature_de_la_restriction,
+    t.restriction_de_hauteur as restriction_de_hauteur,
+    t.restriction_de_poids_total as restriction_de_poids_total,
+    t.restriction_de_poids_par_essieu as restriction_de_poids_par_essieu,
+    t.restriction_de_largeur as restriction_de_largeur,
+    t.restriction_de_longueur as restriction_de_longueur,
+    t.matieres_dangereuses_interdites as matieres_dangereuses_interdites,
+    t.cpx_gestionnaire as cpx_gestionnaire,
+    t.cpx_numero_route_europeenne as cpx_numero_route_europeenne,
+    t.cpx_classement_administratif as cpx_classement_administratif,
+
+    -- géométrie du troncon
+    ST_Force2D(ST_Transform(t.geometrie, 4326)) as geom,
+
+      -- Nouveaux champs initialisés avec valeur par défaut
+    false AS transport_exceptionnel,
+    0::integer AS vla_par_defaut,
+    0::numeric AS cout_penalites,
+    false AS vehicule_leger_interdit,
+    0::numeric AS cout_vehicule_prioritaire
+
+
+  FROM troncon_de_route_local AS t
+  ) as s
+  WHERE s.etat LIKE 'En service'
+  AND s.geom && ST_MakeEnvelope(%(xmin)s,%(ymin)s,%(xmax)s,%(ymax)s, 4326 )
+  -- décommenter pour tester :
+  -- AND territoire='REU'
+;
+
+-- ajout d'index pour la mise à jour
+CREATE INDEX IF NOT EXISTS idx_bduni_troncon_cleabs
+    ON bduni_troncon (cleabs);
+
+CREATE INDEX IF NOT EXISTS idx_troncon_de_route_cleabs
+    ON troncon_de_route_local (cleabs);
+
+-- Mise à jour des nouveaux champs de bduni_troncon avec les valeurs réelles si le champ existe dans la table troncon_de_route.
+-- Si les colonnes n'existent pas dans troncon_de_route : pas de mise à jour des nouveaux champs.
+CREATE TEMP TABLE troncon_columns AS
+SELECT column_name
+FROM information_schema.columns
+WHERE table_schema = '{output_schema}'
+  AND table_name = 'troncon_de_route'
+  AND column_name IN (
+    'transport_exceptionnel',
+    'vla_par_defaut',
+    'cout_penalites',
+    'vehicule_leger_interdit',
+    'cout_vehicule_prioritaire'
+  );
+
+DO $$
+BEGIN
+  -- transport_exceptionnel
+  IF EXISTS (
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'troncon_de_route'
+      AND column_name = 'transport_exceptionnel'
+  ) THEN
+    UPDATE bduni_troncon s
+    SET transport_exceptionnel = t.transport_exceptionnel
+    FROM troncon_de_route_local t
+    WHERE s.cleabs = t.cleabs;
+	END IF;
+
+  -- vla_par_defaut
+  IF EXISTS (
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'troncon_de_route'
+      AND column_name = 'vla_par_defaut'
+  ) THEN
+    UPDATE bduni_troncon s
+    SET vla_par_defaut = t.vla_par_defaut
+    FROM troncon_de_route_local t
+    WHERE s.cleabs = t.cleabs;
+	END IF;
+
+  -- cout_penalites
+  IF EXISTS (
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'troncon_de_route'
+      AND column_name = 'cout_penalites'
+  ) THEN
+    UPDATE bduni_troncon s
+    SET cout_penalites = t.cout_penalites
+    FROM troncon_de_route_local t
+    WHERE s.cleabs = t.cleabs;
+  END IF;
+
+  -- vehicule_leger_interdit
+  IF EXISTS (
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'troncon_de_route'
+      AND column_name = 'vehicule_leger_interdit'
+  ) THEN
+    UPDATE bduni_troncon s
+    SET vehicule_leger_interdit = t.vehicule_leger_interdit
+    FROM troncon_de_route_local t
+    WHERE s.cleabs = t.cleabs;
+  END IF;
+
+  -- cout_vehicule_prioritaire
+  IF EXISTS (
+    SELECT 1 FROM information_schema.columns
+    WHERE table_schema = 'public'
+      AND table_name = 'troncon_de_route'
+      AND column_name = 'cout_vehicule_prioritaire'
+  ) THEN
+    UPDATE bduni_troncon s
+    SET cout_vehicule_prioritaire = t.cout_vehicule_prioritaire
+    FROM troncon_de_route_local t
+    WHERE s.cleabs = t.cleabs;
+  END IF;
+END $$;
+
+
+-- ############################
+-- REMPLISSAGE DE nodes
+-- ############################
+-- Remplissage de nodes avec les sommets initiaux et finaux des tronçons
+-- (les points intermédiaires ne forment pas la topologie)
+WITH t AS (
+  SELECT ST_Force2D(ST_StartPoint(geom)) as geom FROM bduni_troncon
+    UNION
+  SELECT ST_Force2D(ST_EndPoint(geom)) as geom FROM bduni_troncon
+)
+INSERT INTO {output_schema}.nodes (lon,lat,geom)
+  SELECT ST_X(t.geom),ST_Y(t.geom),t.geom FROM t WHERE {output_schema}.nodes_id(t.geom) IS NULL
+;
+
+-- ############################
+-- REMPLISSAGE DE edges
+-- ############################
+
+INSERT INTO {output_schema}.edges
+  SELECT
+    nextval('{output_schema}.edges_id_seq') AS id,
+    {output_schema}.nodes_id(ST_StartPoint(geom)) as source_id,
+    {output_schema}.nodes_id(ST_EndPoint(geom)) as target_id,
+    ST_X(ST_StartPoint(geom)) as x1,
+    ST_Y(ST_StartPoint(geom)) as y1,
+    ST_X(ST_EndPoint(geom)) as x2,
+    ST_Y(ST_EndPoint(geom)) as y2,
+    ST_length(geography(ST_Transform(geom, 4326))) as length_m,
+    (CASE
+      WHEN sens_de_circulation='Sens direct' THEN 1
+      WHEN sens_de_circulation='Sens inverse' THEN -1
+      ELSE 0
+      END) as direction,
+    geom as geom,
+    -- Attributs spécifiques à la bd uni
+    vitesse_moyenne_vl as vitesse_moyenne_vl,
+    nature as nature,
+    cleabs as cleabs,
+    importance as importance,
+    CONCAT('{{"nom_1_gauche": "', nom_1_gauche, '", "nom_1_droite": "', nom_1_droite, '", "cpx_numero": "', cpx_numero, '", "cpx_toponyme": "', cpx_toponyme, '", "nature": "', nature,  '", "importance": "', importance,  '", "cpx_classement_administratif": "', cpx_classement_administratif, '", "restriction_de_hauteur": "', restriction_de_hauteur, '", "restriction_de_poids_total": "', restriction_de_poids_total, '", "restriction_de_poids_par_essieu": "', restriction_de_poids_par_essieu, '", "urbain": "', urbain, '", "cleabs": "', cleabs, '", "cpx_gestionnaire": "', cpx_gestionnaire, '", "vitesse_moyenne_vl": "', vitesse_moyenne_vl, '", "nature_de_la_restriction": "', nature_de_la_restriction, '"}}') as way_names,
+    nom_1_gauche as nom_1_gauche,
+    nom_1_droite as nom_1_droite,
+    cpx_numero as cpx_numero,
+    cpx_toponyme as cpx_toponyme_route_nommee,
+    sens_de_circulation as sens_de_circulation,
+    position_par_rapport_au_sol as position_par_rapport_au_sol,
+    acces_vehicule_leger as acces_vehicule_leger,
+    largeur_de_chaussee as largeur_de_chaussee,
+    nombre_de_voies as nombre_de_voies,
+    insee_commune_gauche as insee_commune_gauche,
+    insee_commune_droite as insee_commune_droite,
+    itineraire_vert as itineraire_vert,
+    reserve_aux_bus as reserve_aux_bus,
+    urbain as urbain,
+    acces_pieton as acces_pieton,
+    nature_de_la_restriction as nature_de_la_restriction,
+    restriction_de_hauteur as restriction_de_hauteur,
+    restriction_de_poids_total as restriction_de_poids_total,
+    restriction_de_poids_par_essieu as restriction_de_poids_par_essieu,
+    restriction_de_largeur as restriction_de_largeur,
+    restriction_de_longueur as restriction_de_longueur,
+    matieres_dangereuses_interdites as matieres_dangereuses_interdites,
+    cpx_gestionnaire as cpx_gestionnaire,
+    cpx_numero_route_europeenne as cpx_numero_route_europeenne,
+    cpx_classement_administratif as cpx_classement_administratif,
+    transport_exceptionnel as transport_exceptionnel,
+    vla_par_defaut as vla_par_defaut,
+    cout_penalites as cout_penalites,
+    vehicule_leger_interdit as vehicule_leger_interdit,
+    cout_vehicule_prioritaire as cout_vehicule_prioritaire
+  FROM bduni_troncon
+;
+
+-- ############################
+-- REMPLISSAGE DE bduni_non_com
+-- ############################
+
+-- On ne conserve que les non communications sur la zone de calcul
+DROP TABLE IF EXISTS {output_schema}.non_comm;
+CREATE TABLE IF NOT EXISTS {output_schema}.non_comm AS
+SELECT * FROM (
+  SELECT
+    cleabs,
+    lien_vers_troncon_entree,
+    -- liens_vers_troncon_sortie
+    regexp_split_to_table(bduni_non_com_tmp.liens_vers_troncon_sortie, E'/') AS lien_vers_troncon_sortie
+  FROM bduni_non_com_tmp
+  WHERE lien_vers_troncon_entree IN (SELECT cleabs from {output_schema}.edges)
+  -- WHERE lien_vers_troncon_entree IN (SELECT cleabs from {output_schema}.edges) AND NOT gcms_detruit
+) AS non_comm_split
+WHERE non_comm_split.lien_vers_troncon_sortie IN (SELECT cleabs from {output_schema}.edges);
+
+-- Remplissage des ids d'edge dans la table des non communications
+ALTER TABLE {output_schema}.non_comm ADD COLUMN IF NOT EXISTS id_from bigint;
+ALTER TABLE {output_schema}.non_comm ADD COLUMN IF NOT EXISTS id_to bigint;
+
+UPDATE {output_schema}.non_comm AS b SET id_from = e.id
+FROM {output_schema}.edges as e
+WHERE e.cleabs = b.lien_vers_troncon_entree
+;
+
+UPDATE {output_schema}.non_comm AS b SET id_to = e.id
+FROM {output_schema}.edges as e
+WHERE e.cleabs = b.lien_vers_troncon_sortie
+;
+
+-- Récupération du point commun entre deux troncons
+CREATE OR REPLACE FUNCTION {output_schema}.common_point(id_from bigint, id_to bigint) RETURNS bigint AS $$
+  SELECT
+  CASE
+    WHEN a.source_id=b.source_id THEN b.source_id
+    WHEN a.source_id=b.target_id THEN b.target_id
+    WHEN a.target_id=b.source_id THEN b.source_id
+    WHEN a.target_id=b.target_id THEN b.target_id
+    ELSE -1
+  END
+  FROM {output_schema}.edges as a, {output_schema}.edges as b
+  WHERE a.id = id_from
+  AND b.id = id_to;
+$$ LANGUAGE SQL ;
+
+ALTER TABLE {output_schema}.non_comm ADD COLUMN IF NOT EXISTS common_vertex_id bigint;
+UPDATE {output_schema}.non_comm SET common_vertex_id = {output_schema}.common_point(non_comm.id_from, non_comm.id_to);
+
+END TRANSACTION;
+VACUUM ANALYZE {output_schema}.non_comm;
+VACUUM ANALYZE {output_schema}.edges;
+VACUUM ANALYZE {output_schema}.nodes;
